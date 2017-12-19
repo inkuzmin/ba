cc7b0d0c4a5c74bbd2139d2990d0f2d128b06bcb
Ivan Kuzmin <inkuzm@gmail.com>
2017-12-19 20:10:40 +0200

    ba5f

---
+++ ba5f/main.go
@@ -0,0 +1,154 @@
+package main
+
+import (
+	"fmt"
+	"flag"
+	"github.com/inkuzmin/bautils"
+)
+
+type Direction int
+
+const (
+	down Direction = iota
+	right
+	diag
+	taxi
+)
+
+const Inf = int(0x7FF0000000000000)
+
+func main() {
+	pam_file := flag.String("p", "", "PAM file")
+	input_file := flag.String("i", "", "Input file")
+
+	flag.Parse()
+
+	scores := bautils.ParsePAM(*pam_file)
+	v, w := bautils.ParseSequences(*input_file)
+
+	score, backtrack, ipenult, jpenult := lcs(v, w, scores, 5)
+
+	fmt.Printf("%v\n", score)
+
+	cmds, va, wa := outputLcs(backtrack, v, w, ipenult, jpenult)
+
+	fmt.Printf("%v\n", cmds)
+	fmt.Printf("%v\n", va)
+	fmt.Printf("%v\n", wa)
+
+}
+
+func outputLcs(backtrack [][]Direction, v string, w string, i int, j int) (string, string, string) {
+
+	var helper func(int,int) string
+
+	va := "" // v aligned
+	wa := "" // w aligned
+
+	// this could be rewritten as a loop with an iterator
+	helper = func (i int, j int) string {
+		if j == 0 && i == 0 { // this set of adhocs could be simplified
+			if backtrack[i][j] == diag {
+				va += string(v[i])
+				wa += string(w[j])
+				return "*"
+			} else if backtrack[i][j] == taxi {
+				return "^"
+			}
+		} else if j < 0 || i < 0 {
+			if j < i {
+				return "+"
+			} else if i < j {
+				return "-"
+			} else {
+				return "*"
+			}
+		}
+		if backtrack[i][j] == down {
+			va += string(v[i])
+			wa += "-"
+			return helper(i - 1, j) + "+"
+		} else if backtrack[i][j] == right {
+			va += "-"
+			wa += string(w[j])
+			return helper(i, j - 1) + "-"
+		} else if backtrack[i][j] == diag {
+			va += string(v[i])
+			wa += string(w[j])
+			return helper(i - 1, j - 1) + "*"
+		} else if backtrack[i][j] == taxi {
+			return "^"
+		} else {
+			panic(-1)
+			return "?"
+		}
+	}
+
+	return helper(i - 1, j - 1), bautils.Reverse(va), bautils.Reverse(wa)
+}
+
+
+
+
+func lcs(v string, w string, scores map[string]map[string]int, penalty int) (int, [][]Direction, int, int) {
+
+	s := make([][]int, len(v)+1)
+
+	for i := range s {
+		s[i] = make([]int, len(w)+1)
+	}
+
+	s[0][0] = 0
+
+	backtrack := make([][]Direction, len(v))
+	for i := range backtrack {
+		backtrack[i] = make([]Direction, len(w))
+	}
+
+	for i := 0; i <= len(v); i++ {
+		s[i][0] = 0
+	}
+
+	for j := 0; j <= len(w); j++ {
+		s[0][j] = 0
+	}
+
+	for i := 1; i <= len(v); i++ {
+		for j := 1; j <= len(w); j++ {
+
+			p := scores[string(v[i - 1])][string(w[j - 1])]
+
+			s[i][j] = bautils.Max4(0, s[i-1][j] - penalty, s[i][j-1] - penalty, s[i-1][j-1] + p)
+
+			var d Direction
+			if s[i][j] == s[i-1][j] - penalty {
+				d = down
+			} else if s[i][j] == s[i][j-1] - penalty {
+				d = right
+			} else if s[i][j] == (s[i-1][j-1] + p) {
+				d = diag
+			} else if s[i][j] == 0 {
+				d = taxi
+			}
+
+			backtrack[i-1][j-1] = d
+		}
+	}
+
+	smax := 0
+	ipenult := 0
+	jpenult := 0
+
+	for i := 0; i <= len(v); i++ {
+		for j := 0; j <= len(w); j++ {
+			if s[i][j] > smax {
+				smax = s[i][j]
+				ipenult = i
+				jpenult = j
+			}
+		}
+	}
+
+	return smax, backtrack, ipenult, jpenult
+}
+
---
+++ bautils/main.go
@@ -0,0 +1,105 @@
+package bautils
+
+import (
+	"os"
+	"fmt"
+	"bufio"
+	"strconv"
+)
+
+func Max(a int, b int) int {
+	if a >= b {
+		return a
+	} else {
+		return b
+	}
+}
+
+func Max3(a int, b int, c int) int {
+	return Max(Max(a, b), c)
+}
+
+func Max4(a int, b int, c int, d int) int {
+	return Max(Max(Max(a, b), c), d)
+}
+
+// Thanks peterSO for an efficient runic solution
+// https://stackoverflow.com/a/4966500
+func Reverse(s string) string {
+	n := len(s)
+	runes := make([]rune, n)
+	for _, r := range s {
+		n--
+		runes[n] = r
+	}
+	return string(runes[n:])
+}
+
+func ParseSequences(file string) (string, string) {
+	var v string
+	var w string
+
+	f, err := os.Open(file)
+
+	if err != nil {
+		fmt.Fprintf(os.Stderr, "error: %v\n", err)
+		panic(err)
+	}
+	scanner := bufio.NewScanner(f)
+
+	l := 0
+	for scanner.Scan() {
+		if l == 0 {
+			v = scanner.Text()
+		}
+		if l == 1 {
+			w = scanner.Text()
+		}
+		l++
+	}
+
+	return v, w
+}
+
+func ParsePAM(file string) map[string]map[string]int {
+	scores := make(map[string]map[string]int)
+
+	f, err := os.Open(file)
+
+	if err != nil {
+		fmt.Fprintf(os.Stderr, "error: %v\n", err)
+		panic(err)
+	}
+
+	scanner := bufio.NewScanner(f)
+
+	scanner.Split(bufio.ScanWords)
+
+	i := 0
+	j := -1
+	letters := make([]string, 0)
+	for scanner.Scan() {
+		line := scanner.Text()
+
+		val, err := strconv.Atoi(line)
+		if err != nil {
+			_, ok := scores[line]
+			if ok {
+				i = 0
+				j++
+				continue
+			} else {
+				letters = append(letters, line)
+				scores[line] = make(map[string]int)
+			}
+		} else {
+			letterH := letters[j]
+			letterV := letters[i]
+			scores[letterH][letterV] = val
+		}
+
+		i++
+	}
+
+	return scores
+}
